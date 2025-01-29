0f65699ab43727e018ca2bc904fd1bc296b84151
D-Walsh33 <62631882+D-Walsh33@users.noreply.github.com>
2025-01-28 20:43:06 -0800

    Updated and added new features
    Program can now do all initial features

--- weightTracker.py
+++ weightTracker.py
@@ -1,14 +1,15 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 # importing date module
 from datetime import date
+from matplotlib.dates import DateFormatter
 
 def track_weight():
     """Tracks weight over time and provides a visualization of your progress."""
     greeting()
     df = load(file_path)
     while True:
-        response = command().upper()
+        response = input('Please enter a command:\n').upper()
         match response:
             case 'L':
                 print('\nlogging!\n')
@@ -17,14 +18,22 @@ def track_weight():
             case 'D':
                 print('\nPrinting!\n')
                 display(df)
+            
+            case 'R':
+                print('Reporting!')
+                report(df)
+
             case 'G':
                 print('\nGraphing!\n')
-                graph()
+                graph(df)
 
             case 'Q':
                 print('\nQuiting!\n')
                 quit(df)
                 break
+
+            case 'H':
+                command()
 
             case _:
                 print('\nInvalid Command\n')
@@ -45,13 +54,38 @@ def display(df):
     """Prints all logs to date"""
     print(df)
 
-def graph():
+def report(df):
+    """Calculate and display a report to the user"""
+    df['Date'] = pd.to_datetime(df['Date'])
+    low = df['Weight'].min()
+    numEntries = df['Weight'].count()
+    firstWeight = df['Weight'].iloc[0]
+    curWeight = df['Weight'].iloc[-1]
+    curLost = firstWeight - curWeight
+    print(f'Your number of weigh-ins: {numEntries}')
+    print(f'Your lowest weight: {low} kg')
+    print(f'Your last weigh-in: {curWeight} kg')
+    print(f'You have currently lost a total of: {curLost} kgs')
+    print(f'Your average weight lost each day: {curLost / numEntries} per day')
+
+
+def graph(df):
     """Creates and displays a graph of current progress"""
-    pass
+    df['Date'] = pd.to_datetime(df['Date'])
+    fig, ax = plt.subplots()
+    ax.plot(df['Date'], df['Weight'])
+    ax.set_xticks(df['Date'])
+    myFmt = DateFormatter('%m - %d')
+    ax.xaxis.set_major_formatter(myFmt)
+    #this tilts the dates
+    fig.autofmt_xdate()
+    ax.set_title('2025 Weight Loss!')
+    plt.show()
+
 
 def quit(df):
     """Saves data to CSV file and quits application"""
-    df.to_csv(file_path, index='Date')
+    df.to_csv(file_path, index=False )
 
 def load(fileName):
     """Loads previous data from local CSV file"""
@@ -60,6 +94,7 @@ def load(fileName):
 def greeting():
     """Greets user and displays available commands."""
     print('Welcome to Weight Tracker 2025!\n')
+    print('Commands: L-D-G-Q-R H For details')
 
 def command():
     """Collects users command"""
@@ -67,8 +102,8 @@ def command():
     print('L: Log a new Weight')
     print('D: Display all records')
     print('G: Graph progress')
+    print('R: Report of progress')
     print('Q: Quit program\n')
-    return input('Please enter a command:\n')
 
 if __name__ == '__main__':
     file_path = "weight.csv"
